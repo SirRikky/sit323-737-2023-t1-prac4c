3709400408ba0f3b072e53b5dc39e2af7f89b442
SirRikky <94730503+SirRikky@users.noreply.github.com>
2023-03-24 18:08:45 +1100

    adds authentication

--- script.js
+++ script.js
@@ -4,17 +4,49 @@ const app = express();
 const winston = require("winston")
 const port = 3040;
 
-// Calculator functions
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const { findUserByUsername } = require('./user-service');
 
-// Validates input as numbers
+// Validation
+
+// Checks for inputs
+const checkInputs = (n1, n2) => {
+    if(n1 === undefined){
+        logger.error("n1 cannot be blank");
+        throw new Error("n1 cannot be blank");
+    }
+    if(n2 === undefined){
+        logger.error("n2 cannot be blank");
+        throw new Error("n2 cannot be blank");
+    }
+}
+const checkInput = (n1) => {
+    if(n1 === undefined){
+        logger.error("n1 cannot be blank");
+        throw new Error("n1 cannot be blank");
+    }
+}
+
+// Validates inputs as numbers
 const verifyNumbers = (n1, n2) => {
+
     if(isNaN(n1)){
-        logger.error("n1 is incorrectly defined");
-        throw new Error("n1 incorrectly defined");
+        logger.error("n1 is needs to be a number");
+        throw new Error("n1 needs to be a number");
     }
     if(isNaN(n2)){
-        logger.error("n2 is incorrectly defined");
-        throw new Error("n2 incorrectly defined");
+        logger.error("n2 is needs to be a number");
+        throw new Error("n2 needs to be a number");
+    }
+}
+const verifyNumber = (n1) => {
+    if(isNaN(n1)){
+        logger.error("n1 is needs to be a number");
+        throw new Error("n1 needs to be a number");
     }
 }
 
@@ -30,6 +62,12 @@ const multiply = (n1, n2) => {
 }
 const divide = (n1, n2) => {
     return n1/n2;
+}
+const square = (n1) => {
+    return n1 ** 2
+}
+const root = (n1) => {
+    return n1 ** 0.5
 }
 
 // Winston logging 
@@ -57,17 +95,58 @@ if (process.env.NODE_ENV !== 'production') {
     }));
 }
 
+// Authentication
+
+app.use(passport.initialize());
+
+const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: 'secret'
+};
+
+// Authenticates user
+passport.use(new JwtStrategy(jwtOptions, (jwt_payload, done) => {
+    const user = findUserByUsername(jwt_payload.username);
+    if (user) {
+        return done(null, user);
+    } else {
+        return done(null, false, { message: 'User not found' });
+    }
+}));
+
 // Calculator endpoints
+
+// Generate token endpoint generates a token for a valid user
+// Example command to make the app work
+// http://localhost:3040/generate-token?username=Rikky
+app.get("/generate-token", (req, res) => {
+
+    try {
+        // Validates username
+        if(findUserByUsername(req.query.username) === null){
+            throw new Error("no user found");
+        }
+    
+        const token = jwt.sign({ username: req.query.username}, 'secret');
+        res.json({ token })
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({statuscode: 500, msg: error.toString() })
+    }
+})
 
 // Add endpoint adds 2 numbers
 // Example command to make the app work
 // http://localhost:3040/add?n1=1&n2=3
-app.get("/add", (req,res) => {
+app.get("/add", passport.authenticate('jwt', { session: false }), (req,res) => {
     try {
+        logger.info('Parameters '+req.query.n1+' and '+req.query.n2+' received for addition');
+
         const n1 = parseFloat(req.query.n1);
         const n2 = parseFloat(req.query.n2);
 
-        logger.info('Parameters '+n1+' and '+n2+' received for addition');
+        checkInputs(req.query.n1, req.query.n2)
         verifyNumbers(n1, n2)
 
         const result = add(n1,n2);
@@ -82,12 +161,14 @@ app.get("/add", (req,res) => {
 // Subtract endpoint subtracts the second number from the first
 // Example command to make the app work
 // http://localhost:3040/subtract?n1=1&n2=3
-app.get("/subtract", (req,res) => {
+app.get("/subtract", passport.authenticate('jwt', { session: false }), (req,res) => {
     try {
+        logger.info('Parameters '+req.query.n1+' and '+req.query.n2+' received for subtraction');
+        
         const n1 = parseFloat(req.query.n1);
         const n2 = parseFloat(req.query.n2);
-        
-        logger.info('Parameters '+n1+' and '+n2+' received for subtraction');
+
+        checkInputs(req.query.n1, req.query.n2)
         verifyNumbers(n1, n2)
 
         const result = subtract(n1,n2);
@@ -102,12 +183,14 @@ app.get("/subtract", (req,res) => {
 // Multiply endpoint multiplies 2 numbers
 // Example command to make the app work
 // http://localhost:3040/multiply?n1=1&n2=3
-app.get("/multiply", (req,res) => {
+app.get("/multiply", passport.authenticate('jwt', { session: false }), (req,res) => {
     try {
+        logger.info('Parameters '+req.query.n1+' and '+req.query.n2+' received for multiplication');
+
         const n1 = parseFloat(req.query.n1);
         const n2 = parseFloat(req.query.n2);
-        
-        logger.info('Parameters '+n1+' and '+n2+' received for multiplication');
+
+        checkInputs(req.query.n1, req.query.n2)
         verifyNumbers(n1, n2)
 
         const result = multiply(n1,n2);
@@ -122,12 +205,14 @@ app.get("/multiply", (req,res) => {
 // Divide endpoint divides the first number by the second
 // Example command to make the app work
 // http://localhost:3040/divide?n1=1&n2=3
-app.get("/divide", (req,res) => {
+app.get("/divide", passport.authenticate('jwt', { session: false }), (req,res) => {
     try {
+        logger.info('Parameters '+req.query.n1+' and '+req.query.n2+' received for division');
+
         const n1 = parseFloat(req.query.n1);
         const n2 = parseFloat(req.query.n2);
         
-        logger.info('Parameters '+n1+' and '+n2+' received for division');
+        checkInputs(req.query.n1, req.query.n2)
         verifyNumbers(n1, n2)
 
         if(n2 == 0){
@@ -144,6 +229,52 @@ app.get("/divide", (req,res) => {
     } 
 });
 
+// Square endpoint squares a number
+// Example command to make the app work
+// http://localhost:3040/square?n1=3
+app.get("/square", passport.authenticate('jwt', { session: false }), (req,res) => {
+    try {
+        logger.info('Parameter '+req.query.n1+' received for squaring');
+        
+        const n1 = parseFloat(req.query.n1);
+
+        checkInput(req.query.n1)
+        verifyNumber(n1)
+
+        const result = square(n1);
+        res.status(200).json({statuscode:200, data: result});
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({statuscode: 500, msg: error.toString() })
+    } 
+});
+
+// Root endpoint finds the square root of a number
+// Example command to make the app work
+// http://localhost:3040/root?n1=25
+app.get("/root", passport.authenticate('jwt', { session: false }), (req,res) => {
+    try {
+        logger.info('Parameter '+req.query.n1+' received for rooting');
+
+        const n1 = parseFloat(req.query.n1);
+
+        checkInput(req.query.n1)
+        verifyNumber(n1)
+
+        if(n1 < 0){
+            logger.error("n2 cannot be negative");
+            throw new Error("n2 cannot be negative");
+        }
+
+        const result = root(n1);
+        res.status(200).json({statuscode:200, data: result});
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({statuscode: 500, msg: error.toString() })
+    } 
+});
 
 // Start the server
 
